615ac3403dcde1525a062f1b554a79d0d54ae570
Ashak Zahin Hasan <azahinhasan@gmail.com>
2024-04-16 16:54:14 +0600

    optimized table of search paitent page and user can go to edit profile now

--- packages/esm-search-patient-app/src/patient-table/patient-table.component.tsx
+++ packages/esm-search-patient-app/src/patient-table/patient-table.component.tsx
@@ -38,18 +38,16 @@ const PatientListDataTable: React.FC<PatientListProps> = ({}) => {
   const layout = useLayoutType();
   const [patients, setPatient] = useState([]);
 
-  const searchPatient = async (e) => {
-    console.log(e.target.value);
-
-    if (e.target.value.length < 3) return;
+  const searchPatient = async (value: string) => {
+    if (value.length < 3) return;
 
     const controller = new AbortController();
-    await findPatients(e.target.value, controller)
+    await findPatients(value, controller)
       .then((response) => {
         console.log('Patients data:', response.data.results);
         if (response.data.results.length > 0) {
           setPatient(response.data.results);
-        }else{
+        } else {
           setPatient([]);
         }
       })
@@ -63,61 +61,53 @@ const PatientListDataTable: React.FC<PatientListProps> = ({}) => {
     console.log(patients, 'patients');
   }, [patients]);
 
-
-  const valueFromAttribute=(attributes,key)=>{
-    const singleAttribute = attributes.find(el=>el.attributeType.display===key)
-    return singleAttribute?.value?singleAttribute.value:"N/A"
-  }
+  const valueFromAttribute = (attributes: any, key: string) => {
+    const singleAttribute = attributes.find((el) => el.attributeType.display === key);
+    return singleAttribute?.value ? singleAttribute.value : 'N/A';
+  };
+  const patientChartUrl = '${openmrsSpaBase}/patient/${patientUuid}/edit';
 
   return (
     <Layer style={{ margin: '0 20px' }}>
-      {/* <Tile> Patient Table</Tile> */}
-
-      {/* <Grid condensed>
-        <Column lg={3}>
-          {' '}
-          <Search />
-        </Column>
-        <Column lg={4}>
-          <Button size="md" kind="tertiary">
-            Search
-          </Button>
-        </Column>
-      </Grid> */}
-      <div className={styles.activeVisitsDetailHeaderContainer}>
-        <div className={!isDesktop(layout) ? styles.tabletHeading : styles.desktopHeading}>
-          <h4>{t('patientList', 'Patient List')}</h4>
-        </div>
-        <div className={styles.backgroundDataFetchingIndicator}>
-          <span>{false ? <InlineLoading /> : null}</span>
-        </div>
+      <div className={!isDesktop(layout) ? styles.tabletHeading : styles.desktopHeading}>
+        <h4>{t('patientList', 'Patient List')}</h4>
       </div>
-      <br />
-      <Search onChange={(e) => searchPatient(e)} />
-      <TableContainer data-testid="encountersTable">
-        <Table>
-          <TableHeader>
-            <TableRow>
-              <TableCell style={{ width: '200px' }}>#</TableCell>
-              <TableCell style={{ width: '200px' }}>Name</TableCell>
-              <TableCell style={{ width: '200px' }}>Phone</TableCell>
-              <TableCell style={{ width: '200px' }}>NID</TableCell>
-              <TableCell>Gender</TableCell>
-            </TableRow>
-            {patients.map((el,i) => {
+      <div className={styles.backgroundDataFetchingIndicator}>
+        <span>{false ? <InlineLoading /> : null}</span>
+      </div>
+      <br/>
+      <div className={styles.activeVisitsContainer}>
+        <Search onChange={(e) => searchPatient(e.target.value)} />
+        <TableContainer data-testid="encountersTable">
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>#</TableCell>
+                <TableCell>Name</TableCell>
+                <TableCell>Phone</TableCell>
+                <TableCell>NID</TableCell>
+                <TableCell>Gender</TableCell>
+              </TableRow>
+            </TableHead>
+            {patients.map((el, i) => {
               return (
-                <TableBody>
-                  <TableCell style={{ width: '200px' }}>{i+1}</TableCell>
-                  <TableCell style={{ width: '200px' }}>{el.person.display}</TableCell>
-                  <TableCell style={{ width: '200px' }}>{valueFromAttribute(el.attributes,"Telephone Number")}</TableCell>
-                  <TableCell style={{ width: '200px' }}>{valueFromAttribute(el.attributes,"NID")}</TableCell>
+                <TableBody style={{height:"40px"}}>
+                  <TableCell>{i + 1}</TableCell>
+                  <TableCell>
+                    {' '}
+                    <ConfigurableLink to={patientChartUrl} templateParams={{ patientUuid: el.uuid }}>
+                      {el.person.display}
+                    </ConfigurableLink>
+                  </TableCell>
+                  <TableCell>{valueFromAttribute(el.attributes, 'Telephone Number')}</TableCell>
+                  <TableCell>{valueFromAttribute(el.attributes, 'NID')}</TableCell>
                   <TableCell>{el.person.gender}</TableCell>
                 </TableBody>
               );
             })}
-          </TableHeader>
-        </Table>
-      </TableContainer>
+          </Table>
+        </TableContainer>
+      </div>
     </Layer>
   );
 };
